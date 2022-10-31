9a9df294b93dcc7599bf1fb235791b2333120e54
laixiaoxiao <yuanlai159911420@icloud.com>
2022-10-31 16:14:00 +0800

    封装configparser方法，获取ini文件中的数据，返回data值

--- util/read_ini.py
+++ util/read_ini.py
@@ -3,15 +3,15 @@ import configparser
 
 class ReadIni(object):
     def __init__(self, ini_path, node):
-        self.ini_path = ini_path
-        self.node = node
-        self.cg = self.load_ini()
+        self.__ini_path = ini_path
+        self.__node = node
+        self.__cg = self.load_ini()
 
     def load_ini(self):
         cg = configparser.ConfigParser()
         """读取文件"""
-        cg.read(self.ini_path)
+        cg.read(self.__ini_path)
         return cg
 
     def get_data(self, key):
-        return self.cg.get(self.node, key)
+        return self.__cg.get(self.__node, key)
