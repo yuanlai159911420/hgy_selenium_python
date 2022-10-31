bd975be9271f10d39796f37303d91c53db50e270
laixiaoxiao <yuanlai159911420@icloud.com>
2022-10-31 15:57:23 +0800

    获取登陆页面元素

---
+++ page/hgy/login_page.py
@@ -0,0 +1,39 @@
+from base.find_element import FindElement
+from selenium.webdriver.remote.webdriver import WebElement
+
+
+class LoginPage(object):
+    def __init__(self, driver, ini_path, node):
+        self.find_element = FindElement(driver, ini_path, node)
+
+    def get_username_element(self, key) -> WebElement:
+        """
+        获取用户名对象
+        :param key:
+        :return:
+        """
+        return self.find_element.get_visibility_element(key)
+
+    def get_password_element(self, key) -> WebElement:
+        """
+        获取密码对象
+        :param key:
+        :return:
+        """
+        return self.find_element.get_element(key)
+
+    def get_login_button_element(self, key) -> WebElement:
+        """
+        获取登录按钮对象
+        :param key:
+        :return:
+        """
+        return self.find_element.get_element(key)
+
+    def get_radio_element(self, key) -> WebElement:
+        """
+        获取radio按钮对象
+        :param key:
+        :return:
+        """
+        return self.find_element.get_element(key)
