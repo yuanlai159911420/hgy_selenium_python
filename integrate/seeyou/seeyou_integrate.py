159a2f02ab04dfcae3c1b815c9592b4abc784c6c
laixiaoxiao <yuanlai159911420@icloud.com>
2022-10-31 17:40:14 +0800

    seeyou系统首页元素操作集成

---
+++ integrate/seeyou/seeyou_integrate.py
@@ -0,0 +1,60 @@
+from handle.seeyou.seeyou_handle import SeeyouHandle
+import os
+
+
+class SeeyouIntegrate(object):
+    def __init__(self, driver):
+        """
+        seeyou系统首页元素操作集成
+        构造函数:
+            ::初始化对象
+        :param driver:
+        """
+        __ini_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))),
+                                  "config/seeyou/seeyou_config.ini")
+        __node = "SeeyouConfig"
+        self.__seeyou_hd = SeeyouHandle(driver, __ini_path, __node)
+
+    def click_collaborative_menu(self):
+        """
+        点击协同工作菜单按钮
+        :return:
+        """
+        key = "collaborative_menu"
+        self.__seeyou_hd.click_navTitleName(key)
+
+    def click_collaborative_sent_menu(self):
+        """
+        点击已发事项菜单按钮
+        :return:
+        """
+        key = "collaborative_sent_menu"
+        self.__seeyou_hd.click_navTitleName(key)
+
+    def click_collaborative_pending_menu(self):
+        """
+        点击待办事项菜单按钮
+        :return:
+        """
+        key = "collaborative_pending_menu"
+        self.__seeyou_hd.click_navTitleName(key)
+
+    def click_project_filing_menu(self):
+        """
+        点击项目备案管理菜单按钮
+        :return:
+        """
+        key = "project_filing_menu"
+        self.__seeyou_hd.click_navTitleName(key)
+
+    def click_project_filing_main_menu(self):
+        """
+        点击项目备案登记表菜单按钮
+        :return:
+        """
+        key = "project_filing_main_menu"
+        self.__seeyou_hd.click_navTitleName(key)
+
+
+if __name__ == '__main__':
+    seeyou_ig = SeeyouIntegrate("123")
